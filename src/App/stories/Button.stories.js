b6b4ddef44b424234bf783c6f0b74da4eb9023d3
Abimael Espinal <abimaelespinal@gmail.com>
2019-06-24 17:25:13 -0400

    added some stories

--- src/App/stories/Button.stories.js
+++ src/App/stories/Button.stories.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import {storiesOf} from '@storybook/react-native';
-import {action} from '@storybook/addon-actions';
+import React, { Component } from "react";
+import { storiesOf } from "@storybook/react-native";
+import { action } from "@storybook/addon-actions";
+import { BufferView } from "./decorators";
+import { Button } from "../components/Button";
 
-import {Button} from '../components/Button';
 
 storiesOf("Button", module)
+.addDecorator(BufferView)
 .add("default", () => (
-    <Button 
-    onPress={action('tapped-default')}
-    >
-    Press Me
-    </Button>
-))
+    <Button onPress={action("tapped-default")}>Press Me</Button>
+  ))
+  .add("outline", () => (
+      <Button onPress={action("tapped-outline")} outline>
+          Press Me
+      </Button>
+  ))
